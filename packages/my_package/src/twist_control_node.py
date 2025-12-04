c437aeccd8479acc961cd59423581d348af9752b
blizzt44 <eliott.ekelund@gmail.com>
2025-12-04 11:03:18 +0100

    Update twist-control

--- packages/my_package/src/twist_control_node.py
+++ packages/my_package/src/twist_control_node.py
@@ -60,27 +60,29 @@ class TwistControlNode(DTROS):
        position = (0,0) #x,y
        axis_length = 10
        switch_var = True 
+       radius = 0.034
+       wheel_circ = radius*2*math.pi
+       Ntot = 137
+       theta = 0
        while not rospy.is_shutdown():
            
            #MPC control  
 
-           if counter > edge_length:
-                self._v = 0.0
-                if counter_2 < right_angle:
-                    self._omega = 4.0
-                    counter_2 += 1
-                else:
-                    self._omega = 0.0
-                    self._v = -0.5
+        #    if counter > edge_length:
+        #         self._v = 0.0
+        #         if counter_2 < right_angle:
+        #             self._omega = 4.0
+        #             counter_2 += 1
+        #         else:
+        #             self._omega = 0.0
+        #             self._v = -0.5
            message = Twist2DStamped(v=self._v, omega=self._omega)
            self._publisher.publish(message)
            counter += 1
-           
-           rospy.loginfo(f"Publishing message: v = '{self._v}', omega = '{self._omega}'") 
+        
            if self._ticks_left is not None and self._ticks_right is not None:
                 left_motor_tick =  self._ticks_left
                 right_motor_tick = self._ticks_right
-                radius = 3.4
                 dr = 0
                 dl = 0
                 d = 0
@@ -89,19 +91,24 @@ class TwistControlNode(DTROS):
                     prev_right_motor_tick = right_motor_tick
                     switch_var = False
                 else: 
-                    dr = right_motor_tick - prev_right_motor_tick
-                    dl = left_motor_tick - prev_left_motor_tick
+                    dNr = right_motor_tick - prev_right_motor_tick
+                    dNl = left_motor_tick - prev_left_motor_tick
+                    dr = wheel_circ*(dNr/Ntot)
+                    dl = wheel_circ*(dNl/Ntot)
                     d = (dr + dl)/2 
                     dtheta = (dr-dl)/axis_length
-                    position = (position[0]+d*math.cos(dtheta),position[1]+d*math.sin(dtheta)) 
+                    theta += dtheta
+                    position = (position[0]+d*math.cos(theta),position[1]+d*math.sin(theta)) 
                     prev_left_motor_tick = left_motor_tick
                     prev_right_motor_tick = right_motor_tick
                 msg = (
                     f"Wheel encoder ticks [position]: "
                     f"{position}"
+                    f" ,Publishing message: v = '{self._v}', omega = '{self._omega}'"
                 ) 
                 rospy.loginfo(msg)
-                rate.sleep()
+           rate.sleep()
+        
 
    def on_shutdown(self):
        stop = Twist2DStamped(v=0.0, omega=0.0)
